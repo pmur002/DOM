1579d00e615ee4cf3fad25074b3c3538cd96a3f8
Paul Murrell <paul@stat.auckland.ac.nz>
2016-06-02 08:59:34 +1200

    some more tests (htmlPage appendChild with callbacks)

--- tests/testthat/test-htmlPage.R
+++ tests/testthat/test-htmlPage.R
@@ -17,6 +17,32 @@ test_that("appendChild works", {
     closePage(headlessPage)
 })
 
+test_that("appendChild with callback", {    
+    headlessPage <- htmlPage(headless=TRUE)
+    result <- NULL
+    appendChild(headlessPage, "<p>test</p>",
+                callback=function(value) { result <<- value })
+    # Call is asynchronous, so pause for it to finish
+    Sys.sleep(.1)
+    expect_equal(result, "<p>test</p>")
+    kill(headlessPage)
+    closePage(headlessPage)
+})
+
+test_that("appendChild with callback with callback", {    
+    headlessPage <- htmlPage(headless=TRUE)
+    result <- NULL
+    appendChild(headlessPage, "<p>test</p>",
+                callback=function(value) {
+                    appendChild(headlessPage, "<p>test2</p>")
+                })
+    # Call is asynchronous, so pause for it to finish
+    Sys.sleep(.2)
+    expect_equal(kill(headlessPage),
+                 "<html><head></head><body><p>test</p><p>test2</p></body></html>")
+    closePage(headlessPage)
+})
+
 test_that("removeChild works", {    
     headlessPage <- htmlPage(headless=TRUE)
     appendChild(headlessPage, "<p>test<p>")
